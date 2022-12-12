6118f04e8683abfac2c34b3c5d381df49fcffe20
iamyasiaa <yana.smirnova.2312@gmail.com>
2022-12-12 12:42:25 +0300

    added function mock and fixed

--- src/index.js
+++ src/index.js
@@ -1,47 +1,58 @@
 import "./style.scss";
-let form = document.forms.myForm;
-let service = form.elements.service;
-let numberService = form.elements.numberService;
-let perconalAccount = form.elements.perconalAccount;
-let theme = form.elements.theme;
-let textAppeal = form.elements.textAppeal;
-let name = form.elements.name;
-let time = form.elements.time;
-let phone = form.elements.phone;
-let email = form.elements.email;
-let area = form.elements.area;
-let locality = form.elements.locality;
-let street = form.elements.street;
-let flat = form.elements.flat;
-let houseNumber = form.elements.houseNumber;
-let button = form.elements.button;
 
-function main() {
-  let promise = new Promise((resolve, reject) => {
+const form = document.forms.myForm;
+function mock(succes, timeout) {
+  const service = form.elements.service;
+  const numberService = form.elements.numberService;
+  const perconalAccount = form.elements.perconalAccount;
+  const theme = form.elements.theme;
+  const textAppeal = form.elements.textAppeal;
+  const name = form.elements.name;
+  const time = form.elements.time;
+  const phone = form.elements.phone;
+  const email = form.elements.email;
+  const area = form.elements.area;
+  const locality = form.elements.locality;
+  const street = form.elements.street;
+  const flat = form.elements.flat;
+  const houseNumber = form.elements.houseNumber;
+
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(
-        JSON.stringify({
-          service: service.value,
-          numberService: numberService.value,
-          perconalAccount: perconalAccount.value,
-          theme: theme.value,
-          textAppeal: textAppeal.value,
-          name: name.value,
-          time: time.value,
-          phone: phone.value,
-          email: email.value,
-          area: area.value,
-          locality: locality.value,
-          street: street.value,
-          flat: flat.value,
-          houseNumber: houseNumber.value,
-        })
-      );
-    }, 1000);
+      if (succes) {
+        resolve(
+          JSON.stringify({
+            service: service.value,
+            numberService: numberService.value,
+            perconalAccount: perconalAccount.value,
+            theme: theme.value,
+            textAppeal: textAppeal.value,
+            name: name.value,
+            time: time.value,
+            phone: phone.value,
+            email: email.value,
+            area: area.value,
+            locality: locality.value,
+            street: street.value,
+            flat: flat.value,
+            houseNumber: houseNumber.value,
+          })
+        );
+      } else {
+        reject("Ошибка");
+      }
+    }, timeout);
   });
-  promise.then((result) => {
+}
+
+function main(ev) {
+  ev.preventDefault();
+  const promise = mock(true, 500);
+
+  promise?.then((result) => {
     alert(result);
   });
+
   return false;
 }
 form.onsubmit = main;
